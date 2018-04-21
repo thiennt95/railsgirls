ff521016fd0b4c4fdff3a3daa4ef2f679d0e6645
thiennt <hoangthaonguyenbg@gmail.com>
2018-04-21 06:54:22 +0000

    ThienNT

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+  root "ideas#index"
+  
+get 'pages/info'
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
